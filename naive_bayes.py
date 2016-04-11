4db123ca3f5a1aeee240ff018b77d842853a1d4b
Bruno Marques <bomarques@inf.ufrgs.br>
2016-04-11 00:36:11 -0300

    Implementation that actually works

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,25 +2,23 @@ import nltk
 import pandas
 
 
-def learn_naive_bayes_text(path='IMDB', targets=('pos', 'neg'),
-                           low=5, high=24004):
+def learn_naive_bayes_text(targets=('pos', 'neg')):
     vocabulary = set()
     target_probabilities = {}
     tokens = {}
     for target in targets:
+        # todos conjuntos possuem o mesmo tamanho
         target_probabilities[target] = 1.0 / len(targets)
-        tokens[target] = []
-        for value in range(low, high+1):
-            text = nltk.data.load('{}/{}/{}.txt'.format(path, target, value),
-                                  format='text')
-            text_tokens = nltk.word_tokenize(text)
-            tokens[target] += text_tokens
-            vocabulary.update(text_tokens)
+        text = nltk.data.load('IMDB/{}.txt'.format(target), format='text')
+        text_tokens = nltk.wordpunct_tokenize(text)
+        tokens[target] = text_tokens
+        vocabulary.update(text_tokens)
     token_probabilities = pandas.DataFrame(index=vocabulary, columns=targets)
     for target in targets:
-        text = nltk.Text(tokens[target])
-        target_denominator = len(tokens[target]) + len(vocabulary)
-        token_probabilities[:, target] = pandas.Series(
-            {token: (text.count(token)+1) / target_denominator
-             for token in vocabulary})
+        frequencies = nltk.FreqDist(tokens[target])
+        token_probabilities.loc[frequencies, target] = frequencies
+        token_probabilities.loc[frequencies, target] += 1
+        token_probabilities.loc[frequencies, target] /= \
+            len(tokens[target]) + len(vocabulary)
+    token_probabilities = token_probabilities.fillna(0)
     return target_probabilities, token_probabilities
